12f1373fdcf32dbbf2029c525f2fa0d6f5637d47
musaubrian <musaubrian45@gmail.com>
2022-09-12 16:01:44 +0300

    add: setup mysql server
    creates a database
    creates a user with all priviledges

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,11 @@
+-- create database
+-- create new user and grant priviledges on 
+-- created database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+GRANT ALL PRIVILGES ON hbnb_dev.*
+    TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.*
+    TO  'hbnb_dev'@'localhost';
