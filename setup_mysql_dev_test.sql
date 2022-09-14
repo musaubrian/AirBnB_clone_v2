f979843b1e1643de38701998d757b1b42d22eb20
musaubrian <musaubrian15@gmail.com>
2022-09-14 12:28:16 +0300

    add: test database setup

---
+++ setup_mysql_dev_test.sql
@@ -0,0 +1,9 @@
+-- setup a new database and user with privileges
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+GRANT ALL PRIVILGES ON hbnb_dev.*
+    TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.*
+    TO  'hbnb_dev'@'localhost';
