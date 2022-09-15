abcab73bed48380e4d3690d5ed5b10ab38f456d9
musaubrian <musaubrian15@gmail.com>
2022-09-15 14:03:36 +0300

    update: update amenities module

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship, relationships
+from sqlalchemy import Column, String, ForeignKey
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """
+    inherits from BaseModel and Base
+    """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable = False)
+    places_amenities = relationship(
+            "Place",
+            secondary="places_amenities"
+            )
