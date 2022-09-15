64a538e2e2e4e779316ffdec1ebca8634093e47c
musaubrian <musaubrian15@gmail.com>
2022-09-15 13:51:28 +0300

    update: follow pycodestyle

--- models/user.py
+++ models/user.py
@@ -9,22 +9,18 @@ from sqlalchemy import Column, String
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    if models.storage_t == 'db':
-        __table_name__ = 'Users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship(
-                "Place", backref="user",
-                cascade="all, delete",
-                passive_deletes=True
-                )
-        reviews = relationship(
-                "Review", backref="user"
-                cascade="all, delete"
-                passive_deletes=True
-                )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship(
+        "Place",
+        backref='user',
+        cascade="all, delete",
+        passive_deletes=True)
+    reviews = relationship(
+        "Review",
+        backref='user',
+        cascade="all, delete",
+        passive_deletes=True)
