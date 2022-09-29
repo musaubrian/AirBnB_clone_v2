0c407c7f05bef0b159e792db5227ae9fa8a4016b
musaubrian <musaubrian15@gmail.com>
2022-09-29 15:03:07 +0300

    update: correctly space sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,6 +9,5 @@ sudo mkdir -p $index_path
 echo "Hello world!!" | sudo tee $index_path/index.html
 sudo ln -sf "/data/web_static/current" "/data/web_static/releases/test/"
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias
-/data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx start
