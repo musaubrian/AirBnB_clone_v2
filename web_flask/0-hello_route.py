75896d5a326541f932a787850e8da085d5b2b492
musaubrian <musaubrian15@gmail.com>
2022-10-13 15:24:47 +0300

    Chore: start a simple flask server

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+creates a simple flask app
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+app.url_map.strict_slashes= False
+
+@app.route("/")
+def hello_hbnb():
+    """
+    returns hello_hbnb
+    """
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
