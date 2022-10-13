e5b7fe6a7abdbac97d8ca99609640ff575b35f88
musaubrian <musaubrian15@gmail.com>
2022-10-13 14:38:13 +0300

    Fix: fix typo in setup_dev

--- setup_mysql_dev_test.sql
+++ setup_mysql_dev_test.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
-GRANT ALL PRIVILGES ON hbnb_dev.*
+GRANT ALL PRIVILEGES ON hbnb_dev.*
     TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.*
     TO  'hbnb_dev'@'localhost';
